5de90e32a551bde041e88bed6d1addfbfbc3113b
The Ceik <ironceikry@gmail.com>
2020-08-16 10:02:00 -0500

    Voting done :^)
    Signed-off-by: The Ceik <ironceikry@gmail.com>

--- Server/src/main/java/Server/core/net/packet/out/SendLink.kt
+++ Server/src/main/java/Server/core/net/packet/out/SendLink.kt
@@ -1,5 +1,7 @@
 package core.net.packet.out
 
+import core.game.node.item.Item
+import core.game.system.task.Pulse
 import core.net.packet.IoBuffer
 import core.net.packet.OutgoingPacket
 import core.net.packet.context.SendLinkContext
@@ -11,5 +13,12 @@ class SendLink : OutgoingPacket<SendLinkContext>{
         var buf: IoBuffer? = null
         buf = IoBuffer(239 + context.linkIndex)
         context.player.session?.write(buf)
+        context.player.sendMessage("Do NOT log out for the next minute! You will not receive your token if you do.")
+        context.player.pulseManager.run(object : Pulse(100){
+            override fun pulse(): Boolean {
+                context.player.inventory.add(Item(13940))
+                return true
+            }
+        })
     }
 }
