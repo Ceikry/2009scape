dd66a1314dfecbfe6e998bd497d7935e2628d964
Badhad <46411657+BadHad@users.noreply.github.com>
2021-02-23 16:42:39 -0500

    Adventure Bots v4.0.0 (Part 1)
    - The Economy & Exploration update!
    
    - Absolutely massive extension to Adventure bots skilling capability opening up endless possibilities.
    
    - Expanded quotes by around 50+ with more relevant quotes.
    
    - Added POI support to better spread out adventures bots throughout the world.
    
    - Improved adventure bot code in places allowing more -smooth- function.
    
    - Increased adventure bots count from 150 to 300 to expedite World 1 and World 2's GE's.
    
    - Expanded ScriptAPI.kt to further expand Adventurer.kt bots toolset.
    
    - Fixed bugs for adventure bots.

--- Server/src/main/java/Server/plugin/ai/general/ScriptAPI.kt
+++ Server/src/main/java/Server/plugin/ai/general/ScriptAPI.kt
@@ -73,6 +73,41 @@ class ScriptAPI(private val bot: Player) {
     }
 
     /**
+     * Gets the nearest node with a name contained in the list of acceptable names
+     * @param acceptedNames the list of accepted npc/object names
+     * @return the nearest node with a matching name or null
+     * @author Ceikry
+     */
+
+    fun getNearestNodeFromList(acceptedNames: List<String>, isObject: Boolean): Node? {
+        if (isObject) {
+            var entity: Node? = null
+            var minDistance = Double.MAX_VALUE
+            for (objects in RegionManager.forId(bot.location.regionId).planes[bot.location.z].objects) {
+                for(e in objects) {
+                    val name = e?.name
+                    if (e != null && acceptedNames.contains(name) && distance(bot, e) < minDistance && !Pathfinder.find(bot, e).isMoveNear && e.isActive) {
+                        entity = e
+                        minDistance = distance(bot, e)
+                    }
+                }
+            }
+            return if(entity == null) null else entity as GameObject
+        } else {
+            var entity: Node? = null
+            var minDistance = Double.MAX_VALUE
+            for (e in RegionManager.forId(bot.location.regionId).planes[bot.location.z].entities) {
+                val name = e?.name
+                if (e != null && acceptedNames.contains(name) && distance(bot, e) < minDistance && !Pathfinder.find(bot, e).isMoveNear) {
+                    entity = e
+                    minDistance = distance(bot, e)
+                }
+            }
+            return entity
+        }
+    }
+
+    /**
      * Gets the nearest node with matching id.
      * @param id the id to look for
      * @param object whether or not the node we are looking for is an object.
@@ -634,6 +669,8 @@ class ScriptAPI(private val bot: Player) {
             Items.RAW_SWORDFISH_371 -> 390
             Items.SHARK_385 -> 720
             Items.RAW_SHARK_383 -> 710
+            Items.TEAK_LOGS_6333 -> 350
+            Items.MAHOGANY_LOGS_6332 -> 847
             else -> null
         }
     }
